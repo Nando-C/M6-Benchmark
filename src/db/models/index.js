d3dbd4337703aff09493bbe85b7487ca0f3e01e1
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-07-16 12:15:01 +0100

    adds Review modal and link to product

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,10 +1,12 @@
 import sequelize from "../config/index.js"
 import Product from "./ProductModel.js"
+import Review from "./ReviewModel.js"
 
-
+Product.hasMany(Review, {foreignKey: {allowNull: false}})
+Review.belongsTo(Product, {foreignKey: {allowNull: false}})
 
 // ================================================================================
 
 
 
-export { sequelize, Product  }
+export { sequelize, Product, Review  }
